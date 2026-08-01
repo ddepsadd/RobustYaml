f35186e6e9348c7c40515a0438df9543ac4aaaa2
depsad <scorpion95445@gmail.com>
2026-08-02 02:22:48 +0300

    Initial commit

---
+++ src/dotnet/ReSharperPlugin.RobustYaml.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.RobustYaml.Tests
+{
+    [ZoneDefinition]
+    public class RobustYamlTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IRobustYamlZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<RobustYamlTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class RobustYamlTestsAssembly : ExtensionTestEnvironmentAssembly<RobustYamlTestEnvironmentZone> { }
+}
